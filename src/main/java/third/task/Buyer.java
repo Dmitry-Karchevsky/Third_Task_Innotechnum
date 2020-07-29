7030c2b975bc3162949d0bf3bbed2917c43f3be9
Dmitry <dima.game2010@yandex.ru>
2020-07-29 15:24:21 +0300

    Конечный вариант

--- src/main/java/third/task/Buyer.java
+++ src/main/java/third/task/Buyer.java
@@ -6,8 +6,6 @@ import java.util.concurrent.CyclicBarrier;
 public class Buyer extends Thread{
     private int products = 0;
     private int operations = 0;
-    //private static boolean availabilityOfProducts = true;
-    //private static boolean sum = 1;
     private static CyclicBarrier barrier;
 
     public static void setCyclicBarrier(int buyers) {
@@ -30,7 +28,6 @@ public class Buyer extends Thread{
         return (int) (Math.random() * 10) + 1;
     }
 
-    // каждый покупатель смотрит на количество товаров на складе
     @Override
     public void run() {
         int tempProductsInOneBuyer;
@@ -48,45 +45,4 @@ public class Buyer extends Thread{
             }
         }
     }
-
-    // покупатель смотрит на переменную availabilityOfProducts, отвечающую за наличие товаров на складе
-    /*@Override
-    public void run() {
-        int tempProductsInOneBuyer;
-        while (availabilityOfProducts){
-            try {
-                barrier.await();
-                tempProductsInOneBuyer = Store.takeProducts(getRandomCount());
-                if (tempProductsInOneBuyer != 0) {
-                    addProducts(tempProductsInOneBuyer);
-                    operations++;
-                }
-                else availabilityOfProducts = false;
-                barrier.await();
-            } catch (InterruptedException | BrokenBarrierException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
-
-    // смотрится сумма товаров у всех покупателей за один поход на склад
-    /*@Override
-    public void run() {
-        while (sum != 0){
-            try {
-                barrier.await();
-                sum = 0;
-                barrier.await();
-            } catch (InterruptedException | BrokenBarrierException e) {
-                e.printStackTrace();
-            }
-
-            int tempProductsInOneBuyer = Store.takeProducts(getRandomCount());
-            sum += tempProductsInOneBuyer;
-            if (tempProductsInOneBuyer != 0) {
-                addProducts(tempProductsInOneBuyer);
-                operations++;
-            }
-        }
-    }*/
 }
